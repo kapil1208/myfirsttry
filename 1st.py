26505ab99c44451739d7448740ef982c12a364dd
kapil1208 <39640842+kapil1208@users.noreply.github.com>
2019-01-19 03:59:14 +0530

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,8 @@
+#even or odd
+a=int(input("enter the month of the year "))
+'''b=int(input("enter 2nd no. "))
+c=int(input("enter 3rd no. "))'''
+if (a==1,3,5,7,8,10,12):
+    print("31 days are in this month")
+else:
+    print("30 days in this month")
